1e63147a83742d1a259da0433cf949e067a81578
jessicaly <jessiclyaruiz@hotmail.com>
2022-03-19 21:27:46 -0500

    segundo commit asincrona

--- Pa_Web_U4/src/main/java/ec/edu/uce/PaWebU4Application.java
+++ Pa_Web_U4/src/main/java/ec/edu/uce/PaWebU4Application.java
@@ -1,16 +1,20 @@
 package ec.edu.uce;
 
+import java.util.concurrent.CompletableFuture;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 import ec.edu.uce.service.IProcesamientoFacultadService;
 import ec.edu.uce.service.ProcesamientoFacultadServiceImpl;
 //matriculacion
 @SpringBootApplication
+@EnableAsync //
 public class PaWebU4Application  implements CommandLineRunner{
 	private static final Logger LOG=LoggerFactory.getLogger(ProcesamientoFacultadServiceImpl.class);
 
@@ -29,12 +33,24 @@ public class PaWebU4Application  implements CommandLineRunner{
 	public void run(String... args) throws Exception {
 		// TODO Auto-generated method stub
 		long inicio=System.currentTimeMillis();
-	Integer valor1 =this.facultadService.calcularDeuda("21313",3);
-	Integer valor2 =this.facultadService.calcularDeuda("78945",6);
-	Integer valor3 =this.facultadService.calcularDeuda("7891",3);
-	Integer valor4 =this.facultadService.calcularDeuda("96423",3);
-	LOG.info("Total"+ (valor1+valor2+valor3+valor4));
+		//procesamiento Asincrono sin manejo de respuesta por cada hilo
+//	Integer valor1 =this.facultadService.calcularDeuda("21313",3);
+//	Integer valor2 =this.facultadService.calcularDeuda("78945",6);
+//	Integer valor3 =this.facultadService.calcularDeuda("7891",3);
+//	Integer valor4 =this.facultadService.calcularDeuda("96423",3);
+//	LOG.info("valor1: "+valor1);
 	
+		//procesamiento Asincrono con manejo de respuesta por cada hilo
+		CompletableFuture<Integer> valor1 =this.facultadService.calcularDeudaFuture("21313",3);
+		CompletableFuture<Integer> valor2 =this.facultadService.calcularDeudaFuture("78945",6);
+		CompletableFuture<Integer> valor3 =this.facultadService.calcularDeudaFuture("7891",3);
+		CompletableFuture<Integer> valor4 =this.facultadService.calcularDeudaFuture("96423",3);
+
+	//esperar a que se preocesen todos los hilos
+		//sentencia que termina de procesar los hilos 
+		CompletableFuture.allOf(valor1,valor2,valor3,valor4).join();
+		
+		LOG.info("Valor Total: "+ (valor1.get()+valor2.get()+valor3.get()+valor4.get()));
 	long tiempoFinal=System.currentTimeMillis();
 	LOG.info("TiempoTranscurrido: "+(System.currentTimeMillis()-inicio)/1000);
 	}
--- Pa_Web_U4/src/main/java/ec/edu/uce/service/ProcesamientoFacultadServiceImpl.java
+++ Pa_Web_U4/src/main/java/ec/edu/uce/service/ProcesamientoFacultadServiceImpl.java
@@ -1,15 +1,18 @@
 package ec.edu.uce.service;
 
+import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.TimeUnit;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 @Service
 public class ProcesamientoFacultadServiceImpl implements IProcesamientoFacultadService{
 
 	private static final Logger LOG=LoggerFactory.getLogger(ProcesamientoFacultadServiceImpl.class);
 	@Override
+	@Async //proceso asincrono
 	public Integer calcularDeuda(String cedula, Integer valor) {
 		LOG.info("Procesando: "+cedula);
 		try {
@@ -18,8 +21,23 @@ public class ProcesamientoFacultadServiceImpl implements IProcesamientoFacultadS
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
+		LOG.info("Finalizo: "+cedula);
 		return valor;
 	}
+	
+	@Override
+	@Async
+	public CompletableFuture<Integer> calcularDeudaFuture(String cedula, Integer valor) {
+		LOG.info("Procesando: "+cedula);
+		try {
+			TimeUnit.SECONDS.sleep(10);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		LOG.info("Finalizo: "+cedula);
+		return CompletableFuture.completedFuture(valor);
+	}
+
 
 }
