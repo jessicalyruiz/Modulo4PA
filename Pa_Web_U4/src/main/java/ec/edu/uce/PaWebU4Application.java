cf42c11efbc8f7bdd456e7744f8d2e1a8cc95f31
jessicaly <jessiclyaruiz@hotmail.com>
2022-03-19 19:17:12 -0500

    Sincrono

--- Pa_Web_U4/src/main/java/ec/edu/uce/PaWebU4Application.java
+++ Pa_Web_U4/src/main/java/ec/edu/uce/PaWebU4Application.java
@@ -1,13 +1,41 @@
 package ec.edu.uce;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import ec.edu.uce.service.IProcesamientoFacultadService;
+import ec.edu.uce.service.ProcesamientoFacultadServiceImpl;
+//matriculacion
 @SpringBootApplication
-public class PaWebU4Application {
+public class PaWebU4Application  implements CommandLineRunner{
+	private static final Logger LOG=LoggerFactory.getLogger(ProcesamientoFacultadServiceImpl.class);
 
+	@Autowired
+	private IProcesamientoFacultadService facultadService;
+	
+	
+	
 	public static void main(String[] args) {
 		SpringApplication.run(PaWebU4Application.class, args);
 	}
 
+
+
+	@Override
+	public void run(String... args) throws Exception {
+		// TODO Auto-generated method stub
+		long inicio=System.currentTimeMillis();
+	Integer valor1 =this.facultadService.calcularDeuda("21313",3);
+	Integer valor2 =this.facultadService.calcularDeuda("78945",6);
+	Integer valor3 =this.facultadService.calcularDeuda("7891",3);
+	Integer valor4 =this.facultadService.calcularDeuda("96423",3);
+	LOG.info("Total"+ (valor1+valor2+valor3+valor4));
+	
+	long tiempoFinal=System.currentTimeMillis();
+	LOG.info("TiempoTranscurrido: "+(System.currentTimeMillis()-inicio)/1000);
+	}
 }
--- Pa_Web_U4/src/main/java/ec/edu/uce/paralelo/hilos/paralelo/MainHilo.java
+++ Pa_Web_U4/src/main/java/ec/edu/uce/paralelo/hilos/paralelo/MainHilo.java
@@ -16,6 +16,8 @@ long tiempoInicial=System.currentTimeMillis();
 		
 		CajeroHilo cajero3=new CajeroHilo("Andres", Arrays.asList(3,2,3,8));
 		
+		
+		System.out.println("Hilo proncipal: "+Thread.currentThread().getName());
 		GestorAtencionHilos gestor=new GestorAtencionHilos(cajero,tiempoInicial);
 		GestorAtencionHilos gestor2=new GestorAtencionHilos(cajero2,tiempoInicial);
 		GestorAtencionHilos gestor3=new GestorAtencionHilos(cajero3,tiempoInicial);
@@ -30,9 +32,9 @@ long tiempoInicial=System.currentTimeMillis();
 		long tiempoTranscurrido=(tiempoFinal-tiempoInicial)/1000;
 		
 		System.out.println("tiempoTranscurrido "+tiempoTranscurrido);
+		System.out.println("Hilo proncipal: "+Thread.currentThread().getName());
 		
-		
-		System.out.println("hilos/-----");
+		System.out.println("termino----");
 	}
 
 }
